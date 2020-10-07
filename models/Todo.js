e68e49906774f550879c5fdb4c2f62db94df83dc
Akinori Sakakibara <gorion.1213@gmail.com>
2020-10-07 22:30:11 +0900

    Created the create method

--- models/Todo.js
+++ models/Todo.js
@@ -24,5 +24,21 @@ for (let i = 0; i < 5; i++) {
 module.exports = {
   findAll: () => {
     return todos.slice();
+  },
+  create: ({title, body}) => {
+    if (!title) {
+      throw new Error('titleは必須です');
+    }
+    if (!body) {
+      throw new Error('bodyは必須です');
+    }
+
+    const todo = new Todo({
+      title: title,
+      body: body,
+    });
+    todos.push(todo);
+
+    return todo;
   }
 };
